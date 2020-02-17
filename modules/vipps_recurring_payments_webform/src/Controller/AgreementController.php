11180ef2e78b7751a0229f00e517085934b78397
Anton Mirzac <anton@netkata.com>
2020-02-17 16:41:42 +0100

    8.x-1.x Added vipps_recurring_payments_webform submodule

---
+++ modules/vipps_recurring_payments_webform/src/Controller/AgreementController.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace Drupal\vipps_recurring_payments_webform\Controller;
+
+use Drupal\Core\Messenger\Messenger;
+use Drupal\vipps_recurring_payments\Service\VippsService;
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Extension\ModuleHandlerInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\webform\Entity\WebformSubmission;
+use Drupal\webform\WebformSubmissionInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class AgreementController extends ControllerBase
+{
+  private $request;
+
+  private $vippsService;
+
+  private $logger;
+
+  protected $messenger;
+
+  /**
+   * The module handler.
+   *
+   * @var \Drupal\Core\Extension\ModuleHandlerInterface
+   */
+  protected $moduleHandler;
+
+  public function __construct(
+    RequestStack $requestStack,
+    VippsService $vippsService,
+    LoggerChannelFactoryInterface $loggerChannelFactory,
+    ModuleHandlerInterface $module_handler,
+    Messenger $messenger
+  )
+  {
+    $this->request = $requestStack->getCurrentRequest();
+    $this->vippsService = $vippsService;
+    $this->logger = $loggerChannelFactory;
+    $this->moduleHandler = $module_handler;
+    $this->messenger = $messenger;
+  }
+
+  public function confirm()
+  {
+    /* @var $submission WebformSubmissionInterface */
+    //TODO use repository for webforms
+    $submission = WebformSubmission::load($this->request->get('submission_id'));
+
+    try {
+      $agreementStatus = $this->vippsService->agreementStatus($submission->getElementData('agreement_id'));
+      $submission->setElementData('agreement_status', $agreementStatus);
+      $submission->resave();
+
+
+      if( $this->vippsService->agreementActive($submission->getElementData('agreement_id'))) {
+        $this->messenger->addMessage($this->t('Subscription has been done successfully'));
+        $this->logger->get('vipps')->info(
+          sprintf("Subscription %s has been done successfully", $submission->getElementData('agreement_id'))
+        );
+      } else {
+        $this->messenger->addError($this->t('Something went wrong. Please contact to administrator'));
+        $this->logger->get('vipps')->error(
+          sprintf("Agreement %s has status %s", $submission->getElementData('agreement_id'), $agreementStatus)
+        );
+      }
+    } catch (\Throwable $e) {
+      $this->logger->get('vipps')->error(sprintf("Agreement %s. ", $submission->get('agreement_id')) . $e->getMessage());
+    }
+
+    return new RedirectResponse('<front>');
+  }
+
+  public static function create(ContainerInterface $container)
+  {
+    /* @var RequestStack $requestStack */
+    $requestStack = $container->get('request_stack');
+
+    /* @var VippsService $vippsService */
+    $vippsService = $container->get('vipps_recurring_payments:vipps_service');
+
+    /* @var $loggerFactory LoggerChannelFactoryInterface */
+    $loggerFactory = $container->get('logger.factory');
+
+    /* @var $moduleHandler ModuleHandlerInterface */
+    $moduleHandler = $container->get('module_handler');
+
+    /* @var Messenger $messenger */
+    $messenger = $container->get('messenger');
+
+    return new static($requestStack, $vippsService, $loggerFactory, $moduleHandler, $messenger);
+  }
+}
---
+++ modules/vipps_recurring_payments_webform/src/Plugin/WebformHandler/VippsAgreementHandler.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Drupal\vipps_recurring_payments_webform\Plugin\WebformHandler;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\vipps_recurring_payments\Factory\RequestStorageFactory;
+use Drupal\vipps_recurring_payments\Repository\ProductSubscriptionRepositoryInterface;
+use Drupal\vipps_recurring_payments\Service\VippsHttpClient;
+use Drupal\webform\Annotation\WebformHandler;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformSubmissionConditionsValidatorInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\Core\Annotation\Translation;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+/**
+ * Form submission handler.
+ *
+ * @WebformHandler(
+ *   id = "vipps_agreement_handler",
+ *   label = @Translation("Vipps agreement handler"),
+ *   category = @Translation("Webform Handler"),
+ *   description = @Translation("This handler submits data to Vipps and stores agreement ID"),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_SINGLE,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_PROCESSED,
+ * )
+ */
+class VippsAgreementHandler extends WebformHandlerBase
+{
+
+  private $httpClient;
+
+  private $requestStorageFactory;
+
+  private $productSubscriptionRepository;
+
+  public function __construct(
+    array $configuration,
+    $plugin_id,
+    $plugin_definition,
+    LoggerChannelFactoryInterface $logger_factory,
+    ConfigFactoryInterface $config_factory,
+    EntityTypeManagerInterface $entity_type_manager,
+    WebformSubmissionConditionsValidatorInterface $conditions_validator,
+    VippsHttpClient $httpClient,
+    RequestStorageFactory $requestStorageFactory,
+    ProductSubscriptionRepositoryInterface $productSubscriptionRepository
+  )
+  {
+    parent::__construct(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $logger_factory,
+      $config_factory,
+      $entity_type_manager,
+      $conditions_validator
+    );
+
+    $this->httpClient = $httpClient;
+    $this->requestStorageFactory = $requestStorageFactory;
+    $this->productSubscriptionRepository = $productSubscriptionRepository;
+  }
+
+  public function confirmForm(array &$form, FormStateInterface $formState, WebformSubmissionInterface $webFormSubmission)
+  {
+    try {
+      $product = $this->productSubscriptionRepository->getProduct();
+      $product->setPrice($this->getAmount($formState));
+
+      $draftAgreementResponse = $this->httpClient->draftAgreement(
+        $this->httpClient->auth(),
+        $this->requestStorageFactory->buildDefaultDraftAgreement(
+          $product,
+          $formState->getValue('phone'),
+          ['submission_id' => $webFormSubmission->id()]
+        )
+      );
+
+      //TODO use repository for webforms
+      $webFormSubmission->setElementData('agreement_id', $draftAgreementResponse->getAgreementId());
+      $webFormSubmission->resave();
+
+      new TrustedRedirectResponse($draftAgreementResponse->getVippsConfirmationUrl(), 302);
+      $redirect = new RedirectResponse($draftAgreementResponse->getVippsConfirmationUrl(), 302);
+      $redirect->send();
+    } catch (\Throwable $exception) {
+      $this->loggerFactory->get('vipps')->error($exception->getMessage());
+    }
+  }
+
+  private function getAmount(FormStateInterface $formState):?float
+  {
+    $amount = !empty($formState->getValue('amount_select')) ? $formState->getValue('amount_select') :
+      $formState->getValue('amount');
+
+    return floatval($amount);
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition)
+  {
+    /* @var $loggerFactory LoggerChannelFactoryInterface */
+    $loggerFactory = $container->get('logger.factory');
+
+    /* @var $configFactory ConfigFactoryInterface */
+    $configFactory = $container->get('config.factory');
+
+    /* @var $entityTypeManager EntityTypeManagerInterface */
+    $entityTypeManager = $container->get('entity_type.manager');
+
+    /* @var $conditionsValidator WebformSubmissionConditionsValidatorInterface */
+    $conditionsValidator = $container->get('webform_submission.conditions_validator');
+
+    /* @var $httpClient VippsHttpClient */
+    $httpClient = $container->get('vipps_recurring_payments:http_client');
+
+    /* @var RequestStorageFactory $requestStorageFactory */
+    $requestStorageFactory = $container->get('vipps_recurring_payments:request_storage_factory');
+
+    /* @var ProductSubscriptionRepositoryInterface $productSubscriptionRepository */
+    $productSubscriptionRepository = $container->get('vipps_recurring_payments:product_subscription_repository');
+
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $loggerFactory,
+      $configFactory,
+      $entityTypeManager,
+      $conditionsValidator,
+      $httpClient,
+      $requestStorageFactory,
+      $productSubscriptionRepository
+    );
+  }
+}
